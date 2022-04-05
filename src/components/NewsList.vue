778b0ca0c351af954314ab6a32651cdd11252233
TiaDavis <tiajhaedavis258@gmail.com>
2022-04-04 20:50:05 -0500

    Updated newslist view

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,14 +1,36 @@
 <template>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
+ <div id="articleP">
+    <div v-for= "article in articles" class="card" style="width: 18rem;">
+        <img class="card-img-top" v-bind:src=article.urlToImage>
+        <div class="card-body">
+            <h5 class="card-title">{{ article.title }}</h5>
+            <p class="card-text">{{article.description}}</p>
+        </div>
+    </div>
+</div>
 </template>
 <script>
 export default {
  data() {
- return {};
+ return {
+ articles: []
+ };
  },
+ created() {
+    let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us', 
+{
+    headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+ }
+})
+    .then(function(response) {
+    return response.json();
+ })
+    .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+ });
+ }
 };
 </script>
